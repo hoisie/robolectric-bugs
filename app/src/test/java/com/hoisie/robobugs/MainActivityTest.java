446853e3b5ae4f3fbdd465b67fa5242b383172c7
Michael Hoisie <hoisie@google.com>
2016-11-21 17:47:52 -0800

    Add a test for checking a Notification icon

--- app/src/test/java/com/hoisie/robobugs/MainActivityTest.java
+++ app/src/test/java/com/hoisie/robobugs/MainActivityTest.java
@@ -1,9 +1,15 @@
 package com.hoisie.robobugs;
 
+import static org.junit.Assert.assertEquals;
+
+import android.app.Notification;
+import android.support.v7.app.NotificationCompat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
 @RunWith(RobolectricTestRunner.class)
@@ -12,5 +18,8 @@ public class MainActivityTest {
     @Test
     public void testMainActivity() throws Exception {
         Robolectric.setupActivity(MainActivity.class);
+        Notification notification = new NotificationCompat.Builder(RuntimeEnvironment
+                .application).addAction(android.R.drawable.ic_delete, "title", null).build();
+        assertEquals(android.R.drawable.ic_delete, notification.actions[0].icon);
     }
 }
