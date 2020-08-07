53578b3a9e11a8964cf440b78cceeae083606105
Michael Hoisie <hoisie@google.com>
2020-08-07 13:09:25 -0700

    Update for Android Studio 4.0

--- app/src/test/java/com/hoisie/robobugs/MainActivityTest.java
+++ app/src/test/java/com/hoisie/robobugs/MainActivityTest.java
@@ -1,16 +1,20 @@
 package com.hoisie.robobugs;
 
+import android.widget.TextView;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(RobolectricTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 23)
 public class MainActivityTest {
     @Test
     public void testMainActivity() throws Exception {
-        Robolectric.setupActivity(MainActivity.class);
+        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
+        assertEquals("Hello World!", ((TextView)activity.findViewById(R.id.main_text)).getText());
     }
 }
