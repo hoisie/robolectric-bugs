da44443895c4c1ada47216428812dcdc2053b431
Michael Hoisie <hoisie@google.com>
2022-04-27 10:18:14 -0700

    Update to 4.8-alpha-1

--- app/src/test/java/com/hoisie/robobugs/MainActivityTest.java
+++ app/src/test/java/com/hoisie/robobugs/MainActivityTest.java
@@ -1,20 +1,30 @@
 package com.hoisie.robobugs;
 
-import android.widget.TextView;
+import static com.google.common.truth.Truth.assertThat;
 
+import android.database.CursorWindow;
+import android.widget.TextView;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
-
-import static org.junit.Assert.assertEquals;
+import org.robolectric.annotation.SQLiteMode;
 
 @RunWith(RobolectricTestRunner.class)
+@Config(sdk = 30)
 public class MainActivityTest {
-    @Test
-    public void testMainActivity() throws Exception {
-        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
-        assertEquals("Hello World!", ((TextView)activity.findViewById(R.id.main_text)).getText());
-    }
+  @Test
+  public void testMainActivity() throws Exception {
+    MainActivity activity = Robolectric.setupActivity(MainActivity.class);
+    assertThat(((TextView) activity.findViewById(R.id.main_text)).getText())
+        .isEqualTo("Hello World!");
+  }
+
+  @SQLiteMode(SQLiteMode.Mode.NATIVE)
+  @Test
+  public void testCursorWindow() {
+    CursorWindow cursorWindow = new CursorWindow("test");
+    cursorWindow.close();
+  }
 }
